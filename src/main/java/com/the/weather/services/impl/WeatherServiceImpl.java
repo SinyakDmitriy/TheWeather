b0c9eb0abe25fdf3a299c72ad34fbeabc006c745
SinyakDmitriy <sinyak.dm.vd@gmail.com>
2019-01-21 17:10:55 +0200

    fixed migrations

--- src/main/java/com/the/weather/services/impl/WeatherServiceImpl.java
+++ src/main/java/com/the/weather/services/impl/WeatherServiceImpl.java
@@ -35,30 +35,51 @@ public class WeatherServiceImpl implements WeatherService {
         weather.setPositionByLatLng(lat, lng);
 
         Map<String, Object> main = (Map<String, Object>) map.get("main");
-        weather.setTemperature((Double) main.get("temp"));
-        weather.setPressure((Integer) main.get("pressure"));
-        weather.setHumidity((Integer) main.get("humidity"));
-        weather.setTempMax((Double) main.get("temp_max"));
-        weather.setTempMin((Double) main.get("temp_min"));
+        weather.setTemperature(getDouble(main.get("temp")));
+        weather.setPressure(getInteger(main.get("pressure")));
+        weather.setHumidity(getInteger(main.get("humidity")));
+        weather.setTempMax(getDouble(main.get("temp_max")));
+        weather.setTempMin(getDouble(main.get("temp_min")));
 
-        weather.setVisibility((Integer) map.get("visibility"));
+        weather.setVisibility(getInteger(map.get("visibility")));
 
         Map<String, Object> wind = (Map<String, Object>) map.get("wind");
-        weather.setWind_speed((Integer) wind.get("speed"));
-        weather.setWind_deg((Integer) wind.get("deg"));
+        weather.setWind_speed(getInteger(wind.get("speed")));
+        weather.setWind_deg(getInteger(wind.get("deg")));
 
         Map<String, Object> clouds = (Map<String, Object>) map.get("clouds");
-        weather.setCloudsAll((Integer) clouds.get("all"));
-
-        weather.setTimestamp((Integer) map.get("dt"));
+        weather.setCloudsAll(getInteger(clouds.get("all")));
+        weather.setTimestamp(getInteger(map.get("dt")));
 
         Map<String, Object> sys = (Map<String, Object>) map.get("sys");
-        weather.setSunrise((Integer) sys.get("sunrise"));
-        weather.setSunset((Integer) sys.get("sunset"));
+        weather.setSunrise(getInteger(sys.get("sunrise")));
+        weather.setSunset(getInteger(sys.get("sunset")));
 
         weather.setName((String) map.get("name"));
 
         return weather;
+    }
+
+    private Double getDouble(Object obj){
+
+        if(obj instanceof Double) return (Double) obj;
+        if(obj instanceof Long) return ((Long) obj).doubleValue();
+        if(obj instanceof Integer) return ((Integer) obj).doubleValue();
+        if(obj instanceof Short) return ((Short) obj).doubleValue();
+        if(obj instanceof Byte) return ((Byte) obj).doubleValue();
+
+        return 0.0d;
+    }
+
+    private Integer getInteger(Object obj){
+
+        if(obj instanceof Integer) return (Integer) obj;
+        if(obj instanceof Long) return ((Long) obj).intValue();
+        if(obj instanceof Double) return ((Double) obj).intValue();
+        if(obj instanceof Short) return ((Short) obj).intValue();
+        if(obj instanceof Byte) return ((Byte) obj).intValue();
+
+        return 0;
     }
 
     @Override
